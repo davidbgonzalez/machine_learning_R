49c2d8469ab746f77a72470a2d16b13ca248e13e
David Gonzalez <davidbgonzalez@gmail.com>
2013-04-25 15:59:18 -0600

    Initialized repo and added warmUpExercise.R and ex1.R

---
+++ README.md
@@ -0,0 +1,9 @@
+Machine Learning Examples with R
+================================
+
+This repo contains examples from the Coursera Machine Learning course in
+R
+
+Good Luck!
+
+dbg
---
+++ warmUpExercise.R
@@ -0,0 +1,22 @@
+# warmUpExercise.R
+warmUpExercise <- function(){
+# WARMUPEXERCISE Example fucntion in R
+
+A <<- array() # <<- is a global and <- is local
+
+# ============= YOUR CODE HERE ==============
+# Instructions: Return the 5x5 identity matrix 
+#               In octave, we return values by defining which variables
+#               represent the return values (at the top of the file)
+#               and then set them accordingly. 
+
+# Type this at your prompt for a hint: ?diag
+A <<- diag(5)
+A
+
+
+
+# ===========================================
+
+}
+
